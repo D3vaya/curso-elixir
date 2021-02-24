429871f0dd25e2f75d29e4e611cae89691e61989
Jc Ayala <j.ayala.higuera@gmail.com>
2021-02-24 14:31:35 -0300

    proyecto mix

--- lib/curso_elixir.ex
+++ lib/curso_elixir.ex
@@ -1,17 +1,4 @@
 defmodule CursoElixir do
-  @moduledoc """
-  Documentation for `CursoElixir`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CursoElixir.hello()
-      :world
-
-  """
   def hello do
     :world
   end
---
+++ lib/writer.ex
@@ -0,0 +1,35 @@
+defmodule Empresa.Writer do
+  # podriamos usar una guarda para validar que el sueldo sea numerico
+  defp to_map(%Empresa.Empleado{nombre: n, posicion: p, sueldo: s}) do
+    %{"nombre" => n, "posicion" => p, "sueldo" => s}
+  end
+
+  defp to_map(_x) do
+    nil
+  end
+
+  defp to_json(m) do
+    {:ok, json_version} = Jason.encode(m)
+    json_version
+  end
+
+  defp dump(str) do
+    File.write("empleados.json", str)
+  end
+
+  def write(list_empleados) do
+    list_empleados
+    |> Enum.map(&to_map/1)
+    |> Enum.filter(fn
+      nil -> false
+      _ -> true
+    end)
+    |> to_json()
+    |> dump()
+  end
+end
+
+# e1 = %Empresa.Empleado{nombre: "JC", posicion: "DevMamager", sueldo: 30000}
+# e2 = %Empresa.Empleado{nombre: "Rod", posicion: "Frontedn", sueldo: 40000}
+# e3 = %Empresa.Empleado{nombre: "Nico", posicion: "Backedn", sueldo: 80000}
+# e4 = %Empresa.Empleado{nombre: "Ado", posicion: "DevOps", sueldo: 50000}
