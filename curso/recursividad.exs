4345c305a41ef5dde5a239a5d1842356a80518fe
Jc Ayala <j.ayala.higuera@gmail.com>
2021-02-24 13:05:26 -0300

    recursividad 2

--- curso/recursividad.exs
+++ curso/recursividad.exs
@@ -10,3 +10,22 @@ defmodule Recursive do
 end
 
 IO.puts(Recursive.factorial(5))
+
+defmodule RecursividadListas do
+
+  def dobles([]), do: []
+
+  #def dobles([head | tail]), do: [head * 2 | dobles(cola)]
+
+  def dobles([head | tail]) do
+    IO.inspect(head)
+    [head * 2 | dobles(tail)]
+  end
+
+  def pares([]), do: []
+  def pares([head | tail]) when rem(head, 2) == 0, do: [head | pares(tail)]
+  def pares([head | tail]), do: pares(tail)
+end
+list = [1, 2, 3, 4]
+IO.puts(RecursividadListas.pares(list))
+IO.puts(RecursividadListas.dobles(list))
